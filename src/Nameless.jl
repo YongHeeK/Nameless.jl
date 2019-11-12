bdbb97233389bd3dfd622402f5c4e6dc9d1b18c9
김용희 <yongheekim@devsisters.com>
2019-11-12 10:26:42 +0900

    update setup!

--- src/Nameless.jl
+++ src/Nameless.jl
@@ -1,10 +1,33 @@
 module Nameless
 
-print("""
-    Fear names. Names have power in identity.
-    Others can use names as weapons.
-    Names are a hook that can be used to track you across the planes.
-    Remain nameless, and you shall be safe.
-""")
+function __init__()
+    print("""
+        메뉴얼을 참고하여 경로를 다음의 함수를 실행해 주세요
+        setup!("내경로")
+    """)
+end
+
+function setup!(repo)
+    if !isdir(repo) 
+        throw(AssertionError("\"$repo\" 경로가 올바르지 않습니다\n올바른 경로를 입력해 주세요"))
+    else
+        p2 = joinpath(repo, "submodules")
+        @assert isdir(p2) "\"$repo\" 경로가 우리 경로가 아닙니다\n정확한 저장소 경로를 확인해 주세요"
+    end
+            
+    startup = joinpath(DEPOT_PATH[1], "config/startup.jl")
+    write(startup, """
+    push!(LOAD_PATH, $p2)""")
+
+    @info "startup.jl을 생성하였습니다"
+        
+
+    juno_startup = joinpath(DEPOT_PATH[1], "config/juno_startup.jl")
+    write(juno_startup, """using GameDataManager""")
+
+    @info "juno_startup.jl을 생성하였습니다"
+end
+
+export setup!
 
 end # module
